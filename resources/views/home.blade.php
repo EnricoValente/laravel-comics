00138fd46a256bc4792e823c7c0db1083bc1254a
EnricoValente <enricovalente42@gmail.com>
2023-09-11 11:31:34 +0200

    edit array in pagina

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -6,7 +6,32 @@ Homepage
 
 
 @section('main-content')
-<h1>
-    Template repository
-</h1>
+@foreach ($comics as $comic)
+<div class="container">
+    <div class="row">
+        <div class="col-3">
+            <div class="card">
+                <img class="card-img-top" src="{{$comic['thumb']}}" alt="">
+            </div>
+
+            <div class="card-body">
+                <h4>
+                    {{$comic['title']}}
+                </h4>
+
+                <p>
+                    {{$comic['description']}}
+                </p>
+
+                <div>
+                    {{$comic['price']}}
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+   
+@endforeach
+    
+
 @endsection
--- routes/web.php
+++ routes/web.php
@@ -14,7 +14,10 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('home');
+    $comics = config('comics');
+
+    $data = compact('comics');
+    return view('home', $data);
 });
 
 
